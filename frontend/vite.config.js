b35f354977eb0922fedb79032ef8ea058c2db2c3
MrsRafiaShahid <mrssrafiashahid@gmail.com>
2025-07-18 00:15:02 +0500

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
+	base: "/",
+	build:{
+		outDir: "dist",
+		emptyOutDir: true,
+	},
+	// Proxy API requests to the backend server
     proxy: {
       "/api": {
         target: "http://localhost:5000",
