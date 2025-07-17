f11584fe033ca134353c80cb25b328deca44dd12
MrsRafiaShahid <mrssrafiashahid@gmail.com>
2025-07-18 02:25:16 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,9 @@ import { v2 as cloudinary } from "cloudinary";
 // Load environment variables from .env file
 dotenv.config();
 cloudinary.config({
-  cloud_name: process.env.Cloudinary_Cloud_Name,
-  api_key: process.env.Cloudinary_API_KEY,
-  api_secret: process.env.Cloudinary_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
