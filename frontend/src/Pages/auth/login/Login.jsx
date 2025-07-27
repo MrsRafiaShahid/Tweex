ade4ac6abca6eb5ac9705a1c0e39f2525fbbe3c1
MrsRafiaShahid <mrssrafiashahid@gmail.com>
2025-07-28 02:52:05 +0500

    add title

--- frontend/src/Pages/auth/login/Login.jsx
+++ frontend/src/Pages/auth/login/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
           <XSvg className="w-full h-full" />
           <span className=" flex flex-col absolute top-28 md:top-35 ">
             <span className="text-14 text-center font-serif font-bold text-violet-500">TWEEX</span>
-            <span className="text-xs font-mono text-blue-300">Say it. Like it. Tweex it</span>
+            <span className="text-[10px] font-mono text-blue-300">Say it. Like it. Tweex it</span>
           </span>
         </div>
       </div>
